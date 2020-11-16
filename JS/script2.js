546e9d98f25f96f9756aa9133a333ccca0c2256d
Mia-Yang <siyuyang1995@gmail.com>
2020-11-16 15:13:46 +0800

    [yang siyu]feat: should filter todos

--- JS/script2.js
+++ JS/script2.js
@@ -3,9 +3,13 @@ var addBtn = document.querySelector(".add-btn");
 var list = document.querySelector(".todo-list");
 var filter = document.querySelector(".filter");
 var clear = document.querySelector(".clear");
+var checkbox = document.querySelector('input[type="checkbox"]');
 var listData = [];
 
 addBtn.addEventListener('click', addTodo);
+clear.addEventListener('click', clearAll);
+filter.addEventListener('click', filterTodo);
+
 
 function addTodo(e) {
     e.preventDefault();
@@ -15,19 +19,88 @@ function addTodo(e) {
         text: itemContent,
         checked: false
     }
-    console.log(newTodo);
     if (itemContent !== null && itemContent !== "" && itemContent !== undefined) {
         listData.push(newTodo);
-        console.log(listData);
-        renderList(listData);
+        filterTodo();
     }
 }
 
 function renderList(data) {
     var html = '';
-    for (var i = 0; i < listData.length; i++) {
-        html += '<li><input type="checkbox"><span>' +
-            data[i].text + '</span><button><i class="fas fa-trash"></i></button></li>'
+    for (var i = 0; i < data.length; i++) {
+        html += ('<li><input type="checkbox"><span>' +
+            data[i].text + '</span><button><i class="fas fa-trash"></i></button></li>')
     }
     list.innerHTML = html;
+}
+
+function filterTodo() {
+    var filterOption = getValue();
+    var activeList = [];
+    var completedList = [];
+    for (var i = 0; i < listData.length; i++) {
+        if (listData[i].checked) {
+            completedList.push(listData[i]);
+        } else {
+            activeList.push(listData[i]);
+        }
+    }
+
+    switch (filterOption) {
+        case "all":
+            renderList(listData);
+            break;
+        case "completed":
+            renderList(completedList);
+            break;
+        case "active":
+            renderList(activeList);
+            break;
+    }
+
+    // if (filterOption === "all") {
+    //     renderList(listData);
+    // }
+    // if (filterOption === "active") {
+    //     for (var i = 0; i < listData.length; i++) {
+    //         activeList = [];
+    //         if (!listData[i].checked) {
+    //             activeList.push(listData[i]);
+    //             console.log(activeList)
+    //             renderList(activeList);
+    //         }
+    //     }
+    // }
+    // if (filterOption === "completed") {
+    //     for (var i = 0; i < listData.length; i++) {
+    //         if (listData[i].checked) {
+    //             completedList = [];
+    //             completedList.push(listData[i]);
+    //             renderList(completedList);
+    //         }
+    //     }
+    // }
+}
+
+function finished(e) {
+    var checked = e.target;
+    var data = checked.nextSibling;
+    data.checked = true;
+    filterTodo();
+}
+
+function getValue() {
+    var value;
+    var radios = document.getElementsByName("option");
+    for (radio of radios) {
+        if (radio.checked) {
+            value = radio.value;
+        }
+    }
+    return value;
+}
+
+function clearAll() {
+    listData = [];
+    renderList(listData);
 }
